08ef7cebb25e2c80cecf9fe8c6d89624e550b54e
Andrejs Gasilovs <gattgas@gmail.com>
2017-08-09 13:46:20 +0300

    Modified Assembly Blinky Project

--- AssemblyLedBlinker/main.asm
+++ AssemblyLedBlinker/main.asm
@@ -23,8 +23,17 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 ;-------------------------------------------------------------------------------
 ; Main loop here
 ;-------------------------------------------------------------------------------
-
-                                            
+			BIS.B	#BIT0,&P1DIR			; P1.0 as output
+			BIS.B	#BIT7,&P4DIR			; P4.7 as output
+			BIS.B	#BIT0,&P1OUT			; Initially P1.0: High
+			BIC.B	#BIT7,&P4OUT			; Initially P4.7 Low
+Looper		mov.w	#0xffff,R15				; Delay loop
+			XOR.B	#BIT0,&P1OUT
+			XOR.B	#BIT7,&P4OUT
+Decr		DEC.W	R15
+			jz		Looper
+            jmp		Decr
+            nop
 
 ;-------------------------------------------------------------------------------
 ; Stack Pointer definition
